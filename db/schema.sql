ea91cabaf5895687c1fb205ee39d17c2b15a42ed
Jieun Rim <rim.jieun@gmail.com>
2017-05-13 05:47:33 -0400

    saving changes it initial files

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ USE burgers_db;
 CREATE TABLE burgers
 (
   id INT AUTO_INCREMENT NOT NULL,
-  burger_name VARCHAR(255) NOT NULL,
+  burger_name VARCHAR(15) NOT NULL,
   devoured BOOLEAN DEFAULT false,
   date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (id)
